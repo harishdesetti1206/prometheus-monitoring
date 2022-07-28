4fe46770ff7d9eaff76599eaf5216bcf0e173f4e
Harish Desetti <hdesetti@sodimac.cl>
2022-07-27 20:43:44 -0400

    feat: add gitlab ci cd file and s3 backend

--- main.tf
+++ main.tf
@@ -14,4 +14,16 @@ terraform {
     }
   }
   required_version = ">= 1.1.2"
+
+  backend "s3" {
+    bucket         = "hdesettistatefiles"
+    key            = "k8s/cluster/k8s-autoscaling-cluster/prometheusmonitoring/key/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "dynamodb-state-locking-prometheusmonitoring"
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-1:286824333895:cluster/k8s-autoscaling-cluster"
 }
